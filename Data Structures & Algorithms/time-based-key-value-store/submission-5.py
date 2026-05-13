58ed032248fd16732ac5b522772937b800c675ba
Luke Fanguna <47843203+Luke-Fanguna@users.noreply.github.com>
2026-05-13 15:59:14 -0700

    Add: time-based-key-value-store - submission-5

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-5.py
@@ -0,0 +1,22 @@
+class TimeMap:
+
+    def __init__(self):
+        self.mapped = {}
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if key not in self.mapped:
+            self.mapped[key] = []
+        self.mapped[key].append((timestamp, value))
+
+    def get(self, key: str, timestamp: int) -> str:
+        res, vals = "", self.mapped[key] if key in self.mapped else []
+        l, r = 0, len(vals) - 1
+
+        while l <= r:
+            mid = (l + r) // 2
+            if vals[mid][0] <= timestamp:
+                res = vals[mid][1]
+                l = mid + 1
+            else:
+                r = mid - 1
+        return res
